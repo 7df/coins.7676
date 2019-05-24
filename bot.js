26f03f09bf2cc1398a69533567bc72cf21ae208a
7df <42803044+7df@users.noreply.github.com>
2019-05-24 16:12:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,32 +1,35 @@
 const Discord = require("discord.js");
-let coins = require("../coins.json");
+const botconfig = require("../botconfig");
+let purple = botconfig.purple;
+let xp = require("../xp.json");
 
 module.exports.run = async (bot, message, args) => {
-  //!coins
-  if(!coins[message.author.id]){
-    coins[message.author.id] = {
-      coins: 0
-    };
-  }
 
-  let uCoins = coins[message.author.id].coins;
+  if(!xp[message.author.id]){
+   xp[message.author.id] = {
+     xp: 0,
+     level: 1
+  };
+}
+  let curxp = xp[message.author.id].xp;
+  let curlvl = xp[message.author.id].level;
+  let nxtLvlXp = curlvl * 300;
+  let difference = nxtLvlXp - curxp;
 
-
-  let coinEmbed = new Discord.RichEmbed()
+  let lvlEmbed = new Discord.RichEmbed()
   .setAuthor(message.author.username)
-  .setColor("#00FF00")
-  .addField("💸", uCoins);
+  .setColor(purple)
+  .addField("Level", curlvl, true)
+  .addField("XP", curxp, true)
+  .setFooter(`${difference} XP til level up`, message.author.displayAvatarURL);
 
-  message.channel.send(coinEmbed).then(msg => {msg.delete(5000)});
+  message.channel.send(lvlEmbed).then(msg => {msg.delete(5000)});
 
 }
 
 module.exports.help = {
-  name: "coins"
+  name: "level"
 }
-
-
-
 
 
 client.login(process.env.BOT_TOKEN);
